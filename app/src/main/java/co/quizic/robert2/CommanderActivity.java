401628f277cf6539b9cb6b001e16a9a9293bb1ac
Lazy eye developer <gilad@quizic.co>
2015-12-05 21:43:40 +0200

    GUI changes

--- app/src/main/java/co/quizic/robert2/CommanderActivity.java
+++ app/src/main/java/co/quizic/robert2/CommanderActivity.java
@@ -2,10 +2,12 @@ package co.quizic.robert2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CommanderActivity extends AppCompatActivity {
@@ -31,29 +33,68 @@ public class CommanderActivity extends AppCompatActivity {
 
         EditText ll = (EditText) findViewById(R.id.editText);
         String[] commands = ll.getText().toString().split("\n");
+
+        ArrayList<String> sequence = new ArrayList<>();
         for(int i=0;i<commands.length;i++){
             String t = compile(commands[i]);
-            BluetoothConnector.getInstance("").sendMessage(t);
+            Log.d("SENDING:", t);
+            sequence.add(t);
+
         }
+        sequence.add(".");
+
+        final ArrayList<String> seq = sequence;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(String s : seq){
+                    BluetoothConnector.getInstance("").sendMessage(s);
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }).start();
+
+
 
     }
     private HashMap<String,String> commandMap;
     private void setCommandMap(){
         commandMap = new HashMap<>();
-        commandMap.put("forward","A");
-        commandMap.put("back","B");
-        commandMap.put("right","C");
-        commandMap.put("left","D");
+        commandMap.put("forward","c");
+        commandMap.put("back","d");
+        commandMap.put("right","a");
+        commandMap.put("left", "b");
+        commandMap.put("stop",".");
 
     }
 
     private String compile(String c){
+
         String[] cc = c.split(" ");
-        int repeat = Integer.getInteger(cc[1]);
-        String res = "";
-        for(int i=0;i<repeat;i++){
-            res+=commandMap.get(cc[0]);
+
+        try{
+            //Log.d("command",cc[0]);
+            //Log.d("repeat",cc[1]);
+
+            //Log.d("command",commandMap.get(cc[0]));
+            //Log.d("repeat",Integer.parseInt(cc[1].trim())+"");
+
+            int repeat = Integer.parseInt(cc[1]);
+            String res = "";
+            for(int i=0;i<repeat;i++){
+                res+=commandMap.get(cc[0]);
+            }
+            return res;
+        }catch (Exception e){
+            Log.e("COMPILE",c);
+            return "";
         }
-        return res;
+
+
     }
 }
