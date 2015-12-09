6fc3e0b6294b248d642dab0a0d76bf733025bc10
OldGeezer <gilad@quizic.co>
2015-12-09 13:13:47 +0200

    visual updates

---
+++ Arduino/nano/nano.ino
@@ -0,0 +1,49 @@
+#include <SoftwareSerial.h>
+char val;
+int v;
+static const int outputs[] = {4, 5, 6, 7, 8, 9, 10, 11};
+static const uint8_t analog_pins[] = {A0, A1, A2, A3, A4, A5, A6, A7};
+String states = "";
+
+
+SoftwareSerial mySerial(2, 3); // RX, TX
+
+void setup()
+{
+  // Open serial communications and wait for port to open:
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+  mySerial.println("Hello, world?");
+  for (int i = 0; i <= sizeof(outputs); i++) {
+    pinMode(outputs[i], OUTPUT);
+    digitalWrite(outputs[i], HIGH);
+  }
+}
+
+void loop() // run over and over
+{
+  delay(500);
+  states = "";
+  for (int i = 0; i < 8; i++) {
+    states += digitalRead(analog_pins[i]);
+
+  }
+  mySerial.println(states + "#");
+
+  if (mySerial.available()) {
+    val = mySerial.read();
+    for (int i = inputShift; i < inputShift + 4; i++) {
+      digitalWrite(i, LOW);
+    }
+    mySerial.println(val - 65);
+    open(val);
+  } else {
+    val = -1;
+  }
+}
+
+void open(char val) {
+  digitalWrite(val - 65 + inputShift, HIGH);
+  delay(1000);
+  digitalWrite(val - 65 + inputShift, LOW);
+}
